ca268a87a2629365739e08379bfcd5cba8576797
raghad <raghad.k.alsaif@gmail.com>
2023-01-14 17:59:47 +0300

    Initial Commit

---
+++ try/tryApp.swift
@@ -0,0 +1,17 @@
+//
+//  tryApp.swift
+//  try
+//
+//  Created by raghad khalid alsaif on 21/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct tryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
